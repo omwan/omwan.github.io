eecf7c318d723971f29b2640c2a5473140eadf55
Olivia Wan <oliviawan97@gmail.com>
2016-07-09 19:05:34 -0400

    fullscreen top on mobile

--- mainController.js
+++ mainController.js
@@ -6,5 +6,5 @@ app.controller('mainController', ['$scope', '$http', function ($scope, $http) {
     $http.get('links.json').success(function (data) {
         $scope.links = data;
     });
-    $scope.footer = "See other things I've done on my LinkedIn or resume, or contant me via my email!. Made with GitHub Pages and AngularJS. Olivia Wan, 2016";
+    $scope.footer = "See other things I've done on my LinkedIn or resume, or contant me via my email. Made with GitHub Pages and AngularJS. Olivia Wan, 2016";
 }]);
