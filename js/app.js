479e595c7a2179938a66489b205fe341f3e87e4b
Olivia Wan <oliviawan97@gmail.com>
2016-08-30 21:54:49 -0400

    adding files from desktop

--- js/app.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module("myapp", ['ngRoute']);
-
-app.config(function($routeProvider){
-	$routeProvider.when('/', {templateUrl: 'views/projects.html', controller: 'mainController'});
-});
